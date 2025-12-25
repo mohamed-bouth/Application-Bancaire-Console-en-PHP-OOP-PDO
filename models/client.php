a94c0eb19c77af49a142a0ba2832bae2a8519791
mohamed_bouth <mohamedbouth87@gmail.com>
2025-12-25 22:49:12 +0100

    BA-23 create the function of getComptes in client class to show the linked comptes with the class

--- models/client.php
+++ models/client.php
@@ -14,7 +14,23 @@ class client {
         $this->dateDeCreation = $dateDeCreation;
     }
 
-    public function getComptes(){}
+    public function getComptes($pdo){
+        try {
+            $sql = "SELECT * FROM clients cl JOIN comptes cm ON cl.id = cm.client_id WHERE cl.id = :client_id ";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([":client_id"=> $this->id]);
+            $comptes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if(!$comptes){
+                echo "thier no compte !";
+                return;
+            }
+            foreach ($comptes as $compte) {
+                echo "#" . $compte['id'] .  " |nom: " . $compte["nom"] ." | prenom: ". $compte["prenom"] . " | email :" . $compte["email"] ." | solde: ". $compte["solde"] . " | compte_type: " . $compte["compte_type"] . "<br>";
+            }
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
     public function getInformation(){
         echo "#". $this->id ." | nom: ". $this->nom ." | prenom: ". $this->prenom ." | email: ". $this->email ." | date de creation: ". $this->dateDeCreation ;
         echo "<br>";
