710a63ef3cd8258953d03032e813fc11ee51f431
mohamed_bouth <mohamedbouth87@gmail.com>
2025-12-25 10:56:47 +0100

    BA-14 create function of render details of client in client class

--- models/client.php
+++ models/client.php
@@ -1,4 +1,25 @@
 <?php 
 class client {
-           
+    private $id;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $dateDeCreation;
+
+    public function __construct($id , $nom , $prenom , $email , $dateDeCreation){
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->dateDeCreation = $dateDeCreation;
+    }
+
+    public function getComptes(){}
+    public function getInformation(){
+        echo "#". $this->id ." | nom: ". $this->nom ." | prenom: ". $this->prenom ." | email: ". $this->email ." | date de creation: ". $this->dateDeCreation ;
+        echo "<br>";
+    }
+    public function getEmail(){
+        return $this->email;
+    }
 }
