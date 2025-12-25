511e0d2fb43bd867823cb8431c25e3a7c7f33308
mohamed_bouth <mohamedbouth87@gmail.com>
2025-12-25 22:47:00 +0100

    BA-9 create the class of compte courant

--- models/compteCourant.php
+++ models/compteCourant.php
@@ -1,13 +1,47 @@
 <?php
-include "./models/compte.php";
 class compteCourant extends compte {
-    public static $depositFee = 1;
+    private static $depositFee = 1;
+    private static $decouvert_max = - 500;
 
-    public function __construct($id , $clientId){
-        parent::__construct($id , $clientId);
-
+    public function __construct($id , $clientId , $solde){
+        parent::__construct($id , $clientId , $solde);
     }
-    public function deposerSolde($solde){}
-    public function retirerSolde($solde){}
-    public function envoyerSolde($solde){}
+    public function deposerSolde($pdo , $solde){
+        try {
+            $newSolde = $this->solde + $solde - self::$depositFee;
+            $sql = "UPDATE comptes SET solde = :solde WHERE client_id = :client_id AND compte_type = 'courant' ";
+            $stmt = $pdo->prepare($sql);
+            $stmt ->execute([
+                ":solde"=> $newSolde,
+                ":client_id"=> $this->clientId
+            ]);
+            $this->solde = $newSolde;
+        }catch(Exception $e){
+            echo $e;
+        }
+    }
+    public function retirerSolde($pdo , $solde){
+        $newSolde = $this->solde - $solde;
+        echo $newSolde . " = "  . $this->solde . " - " . $solde;
+
+        if($newSolde < self::$decouvert_max){
+            echo "we cant make this process !";
+            return;
+        }
+
+        try{
+            $sql = "UPDATE comptes SET solde = :solde WHERE client_id = :client_id AND compte_type = 'courant' ";
+            $stmt = $pdo->prepare($sql);
+            $stmt ->execute([
+                ":solde"=> $newSolde,
+                ":client_id"=> $this->clientId
+            ]);
+            $this->solde = $newSolde;
+        }catch(Exception $e){
+            echo $e;
+        }
+        $this->solde = $newSolde;
+        echo "solde: " . $this->solde;
+    }
+    public function envoyerSolde($pdo , $solde){}
 }
