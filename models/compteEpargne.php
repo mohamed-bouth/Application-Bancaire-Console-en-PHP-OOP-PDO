7206906bde6bbc81d917899ac39381211d763afe
mohamed_bouth <mohamedbouth87@gmail.com>
2025-12-25 22:47:56 +0100

    BA-10 create the class of compte epargne

---
+++ models/compteEpargne.php
@@ -0,0 +1,47 @@
+<?php
+class compteEpargne extends compte {
+    private static $depositFee = 1;
+    private static $decouvert_max = 0;
+
+    public function __construct($id , $clientId , $solde){
+        parent::__construct($id , $clientId , $solde);
+    }
+    public function deposerSolde($pdo , $solde){
+        try {
+            $newSolde = $this->solde + $solde;
+            $sql = "UPDATE comptes SET solde = :solde WHERE client_id = :client_id AND compte_type = 'epargne' ";
+            $stmt = $pdo->prepare($sql);
+            $stmt ->execute([
+                ":solde"=> $newSolde,
+                ":client_id"=> $this->clientId
+            ]);
+            $this->solde = $newSolde;
+        }catch(Exception $e){
+            echo $e;
+        }
+    }
+    public function retirerSolde($pdo , $solde){
+        $newSolde = $this->solde - $solde;
+        echo $newSolde . " = "  . $this->solde . " - " . $solde;
+
+        if($newSolde < self::$decouvert_max){
+            echo "we cant make this process !";
+            return;
+        }
+
+        try{
+            $sql = "UPDATE comptes SET solde = :solde WHERE client_id = :client_id AND compte_type = 'epargne' ";
+            $stmt = $pdo->prepare($sql);
+            $stmt ->execute([
+                ":solde"=> $newSolde,
+                ":client_id"=> $this->clientId
+            ]);
+            $this->solde = $newSolde;
+        }catch(Exception $e){
+            echo $e;
+        }
+        $this->solde = $newSolde;
+        echo "solde: " . $this->solde;
+    }
+    public function envoyerSolde($pdo , $solde){}
+}
